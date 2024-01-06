9fa7086b23b21522ae6d280ce6e27bbdc16fb651
Zack Elia <github@zacharyelia.com>
2024-01-06 17:13:36 +0000

    Update app config.

--- src/include/CHIPProjectAppConfig.h
+++ src/include/CHIPProjectAppConfig.h
@@ -7,11 +7,14 @@
 #define LOCALSTATEDIR "/tmp/chip"
 #define CHIP_CONFIG_KVS_PATH "/tmp/chip/chip_kvs"
 
+// Found in connectedhomeip/src/include/platform/CHIPDeviceConfig.h
 #define CHIP_DEVICE_CONFIG_DEVICE_VENDOR_NAME "Zack Elia"
 #define CHIP_DEVICE_CONFIG_TEST_SERIAL_NUMBER "serial"
 #define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_NAME "WLED Matter Bridge"
 #define CHIP_DEVICE_CONFIG_DEVICE_SOFTWARE_VERSION_STRING "0.1.0"
+#define CHIP_DEVICE_CONFIG_DEFAULT_DEVICE_HARDWARE_VERSION_STRING "0.1.0"
 
-#define CHIP_DEVICE_CONFIG_DEVICE_NAME "WLED Matter Bridge"
+// TODO: Is this actually used anywhere? Broken for bridges?
+// #define CHIP_DEVICE_CONFIG_DEVICE_NAME "WLED Matter Bridge"
 
 #include <platform/Linux/CHIPPlatformConfig.h>
